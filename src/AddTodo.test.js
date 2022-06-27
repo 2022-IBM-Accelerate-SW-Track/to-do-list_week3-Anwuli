3535d1caf2dff2af2f7bb4c27b7c337d55201dc1
Anwuli <aajabor2020@my.fit.edu>
2022-06-27 18:03:04 -0400

    week3 work

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -21,23 +21,60 @@ afterEach(() => {
 
  test('test that App component doesn\'t render dupicate Task', () => {
   render(<App />);
+    const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+    const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+    const element = screen.getByRole('button', {name: /Add/i});
+    const dueDate = "12/30/2023";
+    fireEvent.change(inputTask, { target: { value: "History Test"}});
+    fireEvent.change(inputDate, { target: { value: dueDate}});
+    fireEvent.click(element);
+    let check = screen.getAllByText(/History Test/i);
+
+    expect(check.length).toBe(1);
+  
  });
 
  test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
- });
+    const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+    const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+    const element = screen.getByRole('button', {name: /Add/i});
+    const dueDate = "05/30/2023";
+    const task = "";
+    fireEvent.change(inputTask, { target: { value: "History Test"}});
+    fireEvent.change(inputDate, { target: { value: dueDate}});
+    fireEvent.click(element);
+    const check = screen.getByText(/History Test/i);
+    const checkDate = screen.getByText(new RegExp(new Date(dueDate).toLocaleString(), "i"));
+    expect(check).toBeInTheDocument();
+    expect(checkDate).toBeInTheDocument();
+   });
+  
+
 
  test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
- });
+    const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+    const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+    const element = screen.getByRole('button', {name: /Add/i});
+    fireEvent.change(inputTask, { target: { value: "History Test"}});
+    fireEvent.change(inputDate, { target: { value: null}});
+    fireEvent.click(element);
+    const check = screen.getByText(/You have no todo's left/i);
+    expect(check).toBeInTheDocument();
+   });
+  
+
 
 
 
  test('test that App component can be deleted thru checkbox', () => {
   render(<App />);
+ 
  });
 
 
  test('test that App component renders different colors for past due events', () => {
   render(<App />);
+  
  });
--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
+import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 class AddTodo extends Component {
   // Create a local react state of the this component with both content date property set to nothing.
   constructor() {
     super();
-    this.state = {
+    this.state = { 
       content: "",
-      date: ""
+      date: "",
+      due : null,
     };
   }
   // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
@@ -16,22 +19,30 @@ class AddTodo extends Component {
   handleChange = (event) => {
     this.setState({
       content: event.target.value,
-      date: Date().toLocaleString('en-US')
+      date: Date().toLocaleString('en-US'),
     });
   };
   // The handleSubmit function collects the forms input and puts it into the react state.
   // event.preventDefault() is called to prevents default event behavior like refreshing the browser.
   // this.props.addTodo(this.state) passes the current state (or user input and current date/time) into the addTodo function defined
   // in the Home.js file which then adds the input into the list.
+
   handleSubmit = (event) => {
     event.preventDefault();
     if (this.state.content.trim()) {
       this.props.addTodo(this.state);
       this.setState({
         content: "",
-        date: ""
+        date: "",
+        due: null
       });
     }
+  };
+  handleDatepicker =(event) => {
+    this.setState({
+      due: Date(event).toLocaleDateString()
+
+    });
   };
   render() {
     return (
@@ -49,6 +60,15 @@ class AddTodo extends Component {
           onChange={this.handleChange}
           value={this.state.content}
         />
+        <LocalizationProvider dateAdapter={AdapterDateFns}>         
+          <DesktopDatePicker
+          id="new-item-date"
+          label="Due Date"
+          value={this.state.due}
+          onChange={this.handleDatepicker}
+          renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
         <Button
           style={{ marginLeft: "10px" }}
           onClick={this.handleSubmit}
